7eefe262226bcf986ac415deda66884d78129ced
Anh Khoa <phunganhkhoa123@gmail.com>
2024-03-18 16:24:51 +0700

    Schema & Resolver trong GraphQL là gì?

--- server.mjs
+++ server.mjs
@@ -1,15 +1,35 @@
 import express from 'express'
 import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import { ApolloServer } from '@apollo/server'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-import bodyParser from 'body-parser'
 import { expressMiddleware } from '@apollo/server/express4'
 
 const app = express()
 const httpServer = http.createServer(app)
 
-const typeDefs = ''
-const resolvers = {}
+const typeDefs = `#graphql
+  type Folder {
+    id: String,
+    name: String,
+    createdAt: String,
+  }
+
+  type Author {
+    id: String,
+    name: String,
+  }
+
+  type Query {
+    folders: [Folder]
+  }
+`
+const resolvers = {
+  Query: {
+    name: () => { return '123 alo' }
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
@@ -22,4 +42,4 @@ await server.start()
 app.use(cors(), bodyParser.json(), expressMiddleware(server))
 
 await new Promise((resolve) => httpServer.listen({port: 4000}, resolve))
-console.log('Server ready at http://localhost:4000')
+console.log('🚀 Server ready at http://localhost:4000')
